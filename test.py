557fb791e8cb4dfaec061728681c93bc0c602d26
ayala117 <kenzebajaala022@gmail.com>
2025-02-06 17:02:35 +0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,11 @@
+#A recipe you are reading states how many
+# grams you need for the ingredient. Unfortunately,
+# your store only sells items in ounces.
+# Create a function to convert grams to ounces.
+# ounces = 28.3495231 * grams
+
+def grams(gr):
+    return gr*28.3495231
+print(grams(100))
+
+#output 2834.9523099999997
